131e6551b6a3440f8832c3b8483d3c7ec965c43b
ELAP-sys <amirparsa793@hotmail.com>
2025-12-24 03:40:13 +0330

    css and auth

---
+++ index.js
@@ -0,0 +1,44 @@
+const express = require('express')
+const path = require('path')
+const app = express()
+const port = 4040
+
+app.use(express.urlencoded({ extended: true }))
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.get('/test', (req, res) => {
+    res.setHeader("Content-Type", "text/html")
+    res.sendFile(path.join(__dirname, 'public', 'login.html'), (err) => {
+        if (err) {
+            console.error(`failed sending, ${err}`)
+        }
+        else {
+            console.log("done!")
+        }
+    })
+})
+
+
+const todos = [
+    "leila leila",
+    "jusus christ"
+]
+
+
+app.post('/test', (req, res) => {
+    res.setHeader("Content-Type", "text/Plain")
+    const { usr, pass } = req.body
+    console.log(req.body)
+    if (usr === "parsaamir" && pass === "2000")
+        res.status(200).send(`profile: ${todos}`)
+    else if (usr === "parsaamir")
+        res.status(403).send("wrong password")
+    else
+        res.status(404).send("not found")
+
+})
+
+
+app.listen(port, () => {
+    console.log(`servver listening on , ${port}`)
+})
---
+++ public/script.js
@@ -0,0 +1,6 @@
+const input = document.getElementById("password")
+const toggle = document.getElementById("togglePassword")
+toggle.addEventListener("click", () => {
+  const isPassword = input.type === "password"
+  input.type = isPassword ? "text" : "password"
+});
